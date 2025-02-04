2aedaf3d241b528e40aefa0ce5ac58987f1273aa
Locvpo1233211 <106452148+Locvpo1233211@users.noreply.github.com>
2025-02-04 11:48:49 +0700

    static file
    cau hinh file public voi stactic file

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const app = express();
 require("dotenv").config();
-
-const port = 3000;
+const app = express();
 const path = require("path");
+const port = process.env.PORT || 3000;
 
-console.log(process.env.PORT);
-
+// config template engine
 app.set("views", path.join(__dirname, "views"));
-
 app.set("view engine", "ejs");
 
+// config static file
+app.use(express.static(path.join(__dirname, "public")));
+
+// khai bao route
 app.get("/", (req, res) => {
     res.render("sampple.ejs");
 });
