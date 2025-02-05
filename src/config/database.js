9e4de741bb880223282a5e6e8259f17886bbb66f
Locvpo1233211 <106452148+Locvpo1233211@users.noreply.github.com>
2025-02-05 20:09:53 +0700

    mvc file database
    cau truc lai file database theo cau truc mvc

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const mysql = require("mysql2");
+
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+module.exports = connection;
