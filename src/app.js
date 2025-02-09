eccfdf2a9820713f5670b0b381bfcb98e01bfb18
Locvpo1233211 <106452148+Locvpo1233211@users.noreply.github.com>
2025-02-09 10:32:45 +0700

    Create Database

--- src/app.js
+++ src/app.js
@@ -7,9 +7,17 @@ const configViewEngine = require("./config/viewEngine");
 const webRoute = require("./route/web");
 // Get the client
 const connection = require("./config/database");
+const mongoose = require("mongoose");
+
 configViewEngine(app);
 app.use("/", webRoute);
 
+const kittySchema = new mongoose.Schema({
+    name: String,
+});
+const Kitten = mongoose.model("Kitten", kittySchema);
+const cat = new Kitten({ name: "hoidan it" });
+cat.save();
 (async () => {
     try {
         await connection();
