14d404f932d4ba64066cf2ea30637c0f9a78f1f4
Locvpo1233211 <106452148+Locvpo1233211@users.noreply.github.com>
2025-02-16 11:40:14 +0700

    validate data with joi

--- src/controller/customerController.js
+++ src/controller/customerController.js
@@ -9,46 +9,61 @@ const {
     deleteAllCustomerService,
 } = require("../service/customerService");
 const aqp = require("api-query-params");
+const Joi = require("joi");
 module.exports = {
     postCreateCustomerAPI: async (req, res) => {
         let { name, address, phone, email, description } = req.body;
-        let imgUrl = "";
-
-        let sampleFile = req.files.image;
-        console.log(">>>>sampleFile", sampleFile);
-        // sampleFile processing logic here
-        if (!req.files || Object.keys(req.files).length === 0) {
-        } else {
-            result = await uploadSingleFileAPI(sampleFile);
-            imgUrl = result.path;
-        }
-        let customerData = {
-            name,
-            address,
-            phone,
-            email,
-            image: imgUrl,
-            description,
-        };
-
-        result = await createCustomerService(customerData);
-        console.log(">>>>result", result);
-
-        // let customer = await Customer.create(customerData);
-        // return res.status(201).json({
-        //     errorCode: 0,
-        //     data: customer,
-        if (result) {
-            return res.status(201).json({
-                errorCode: 0,
-                data: result,
-            });
-        } else {
-            return res.status(500).json({
+        const schema = Joi.object({
+            name: Joi.string().min(3).max(30).required(),
+            address: Joi.string().min(3).max(30).required(),
+            phone: Joi.string().min(3).max(30).required(),
+            email: Joi.string().email().required(),
+        });
+        const { error, value } = schema.validate(req.body);
+        if (error) {
+            return res.status(400).json({
                 errorCode: 1,
-                message: "Create customer failed",
-                data: result,
+                message: error.details[0].message,
             });
+        } else {
+            let imgUrl = "";
+
+            let sampleFile = req.files.image;
+            console.log(">>>>sampleFile", sampleFile);
+            // sampleFile processing logic here
+            if (!req.files || Object.keys(req.files).length === 0) {
+            } else {
+                result = await uploadSingleFileAPI(sampleFile);
+                imgUrl = result.path;
+            }
+            let customerData = {
+                name,
+                address,
+                phone,
+                email,
+                image: imgUrl,
+                description,
+            };
+
+            result = await createCustomerService(customerData);
+            console.log(">>>>result", result);
+
+            // let customer = await Customer.create(customerData);
+            // return res.status(201).json({
+            //     errorCode: 0,
+            //     data: customer,
+            if (result) {
+                return res.status(201).json({
+                    errorCode: 0,
+                    data: result,
+                });
+            } else {
+                return res.status(500).json({
+                    errorCode: 1,
+                    message: "Create customer failed",
+                    data: result,
+                });
+            }
         }
     },
 
